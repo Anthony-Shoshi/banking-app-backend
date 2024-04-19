7d0bc69738481269f25f417fc5ae73a24d4c5360
Anthony Shoshi <anthonyshoshigomes1996@gmail.com>
2024-04-20 01:05:16 +0200

    init commit

---
+++ src/main/java/com/groupfour/bankingapp/Models/Guitar.java
@@ -0,0 +1,31 @@
+package com.groupfour.bankingapp.Models;
+
+public class Guitar {
+    private long id;
+    private String brand;
+    private String model;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
