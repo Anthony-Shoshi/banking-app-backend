335364fcee3968d85f9897b79b0400002012c588
Muhammad Faizan <701765@student.inholland.nl>
2024-06-10 15:35:09 +0200

    frontend link

--- src/main/java/com/groupfour/bankingapp/Security/WebSecurityConfiguration.java
+++ src/main/java/com/groupfour/bankingapp/Security/WebSecurityConfiguration.java
@@ -110,7 +110,7 @@ public class WebSecurityConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
+        configuration.setAllowedOrigins(Collections.singletonList("https://anthony-shoshi.github.io/banking-app-frontend/"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
